d08ed4a0be2cfefdd24852e2c6fe5486bd789926
cheapache <61108354+cheapache@users.noreply.github.com>
2020-06-19 16:44:00 +0700

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#pdf').on('click', function(){
+       $('.container').printThis({
+        debug: false,               // show the iframe for debugging
+        importCSS: true,            // import parent page css
+        importStyle: true,         // import style tags
+        printContainer: true,       // print outer container/$.selector
+        loadCSS: '../style/index.css',     // path to additional css file - use an array [] for multiple
+        pageTitle: "",              // add title to print page
+        removeInline: false,        // remove inline styles from print elements
+        removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
+        printDelay: 333,            // variable print delay
+        header: null,               // prefix to html
+        footer: null,               // postfix to html
+        base: true,                // preserve the BASE tag or accept a string for the URL
+        formValues: true,           // preserve input/form values
+        canvas: false,              // copy canvas content
+        doctypeString: '...',       // enter a different doctype for older markup
+        removeScripts: false,       // remove script tags from print content
+        copyTagClasses: false,      // copy classes from the html & body tag
+        beforePrintEvent: null,     // function for printEvent in iframe
+        beforePrint: null,          // function called before iframe is filled
+        afterPrint: null            // function called before iframe is removed
+       });
+    });
+});
